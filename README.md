977c051d7a20b3d0eedf4b74aebbf0719c7b0423
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-06-26 23:01:52 +0200

    Add simple README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # study-tracker
+
+Study Tracker is a simple Python application that helps me track my daily study sessions. 
+It measures how long each session lasts, keeps track of my current study streak, and stores the total time I have spent studying.
+
+When the program is closed, it automatically sends a summary to my private Discord channel with:
+
+- study session duration,
+- current study streak,
+- total study time.
+
+The application stores its data in a local JSON file.
+This project was created to help me stay motivated while working on my larger backend projects.
+Before I start studying, I launch the tracker, and when I finish, I stop it. Receiving a summary on Discord makes it easy to monitor my progress and maintain consistency.
--- tracker.py
+++ tracker.py
@@ -1,10 +1,13 @@
 import json
 import time
 from datetime import datetime, timedelta, date
-from dotenv import load_dotenv
-load_dotenv()
 import requests
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
+webhook_url = os.getenv('DC_WEBHOOK_URL')
 
 
 def format_time(time_in_seconds:int) -> str:
@@ -34,6 +37,14 @@ def calculate_streak(last_date:str, streak:int) -> int:
         return streak
     return 1
 
+def send_message(webhook:str, message:str) -> None:
+    body = {
+        "content": message,
+    }
+    response = requests.post(webhook, data=body)
+    if response.status_code != 204:
+        print("Failed to send message!")
+
 
 
 if __name__ == "__main__":
@@ -52,11 +63,20 @@ if __name__ == "__main__":
 
     current_streak = calculate_streak(data["last_date"], data["streak"])
 
+
+
+
     new_data ={
         "last_date": today.strftime("%Y-%m-%d"),
         "streak": current_streak,
         "total_time": data["total_time"] + session,
     }
+
+    message_to_send = (f"Today's you studied for {format_time(int(session))} \n"
+                       f"Your current streak is {current_streak} \n"
+                       f"And you overall time of learning is {format_time(int(new_data['total_time']))} \n"
+                       f"-----------------------------------")
+    send_message(webhook_url, message_to_send)
 
     with open("date.json", "w") as f:
         json.dump(new_data, f)
